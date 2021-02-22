e2f82a66d5d9c5007e55320b576ae2118746e4e3
Phil Goodwin <phil.goodwin@gmail.com>
2021-02-21 18:28:35 -0800

    Updated alias pointer into bash-it
    - gonna have to move to zsh soon

--- scripts/common/aliases_setup.sh
+++ scripts/common/aliases_setup.sh
@@ -1,4 +1,5 @@
-ALIAS_FILE=~/.bash_it/aliases/enabled/general.aliases.bash
+ALIAS_FILE=~/.bash_it/aliases/available/general.aliases.bash
+touch $ALIAS_FILE
 if ! grep -Fxq "#Workstation_alias" ${ALIAS_FILE}; then
   printf "%s\\n" "#Workstation_alias" >> ${ALIAS_FILE}
   printf "%s\\n" "source ${WORKSTATION_SETUP_HOME}/files/aliases.sh" >> ${ALIAS_FILE}
